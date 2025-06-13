f194cc85ec3bd45a9446022c1114109e35143034
Adnan Haider <118106335+AdnanMehr8@users.noreply.github.com>
2025-06-13 22:39:04 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,11 @@ require("dotenv").config();
 const MONGO_URI = process.env.MONGO_URI;
 const dbConnect = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+await mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
     console.log("MongoDB Successfully Connected");
   } catch (error) {
     console.error("Error Connecting DB: ", error);

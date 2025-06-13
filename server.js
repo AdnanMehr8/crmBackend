87e9d93eeebd5a60644f6ca10d380f373bc4bc2d
Adnan Haider <118106335+AdnanMehr8@users.noreply.github.com>
2025-06-13 22:43:50 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,4 @@
-const express = require("express");
-const dbConnect = require("./config/db");
-const customerRoute = require("./routes/customer");
-const userRoute = require("./routes/user");
-const morgan = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
 
-const app = express();
+const app = require("./app");
 
-dbConnect();
-
-app.use(express.json());
-
-app.use(morgan("dev"));
-
-var corsOptions = {
-  origin: "https://crm-humai.vercel.app",
-  optionsSuccessStatus: 200, 
-};
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-app.use(cors(corsOptions)); 
-app.use("/api", customerRoute);
-app.use("/api", userRoute);
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server Running on PORT: ${port}`);
-});
+module.exports = app;
